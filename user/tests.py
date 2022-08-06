2d3ad672213ce6ad9d23ac0724c463b58270cc92
evergreensangrok <evergreensangrok@gmail.com>
2022-08-06 14:56:41 +0900

    modify user test env variable

--- user/tests.py
+++ user/tests.py
@@ -10,5 +10,5 @@ from .models import User
 
 @pytest.mark.django_db
 def test_user_create():
-    User.objects.create(email="candidate@test.com", password=os.environ.get("TEST_USER_PASSWORD"))
+    User.objects.create(email="candidate@test.com", password=os.environ.get("CI_TEST_PASS"))
     assert User.objects.count() == 1
